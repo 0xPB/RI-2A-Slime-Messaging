badf60ba260cc51f2e7cc2bde1e4b933abef3837
Paul Bruno <157818272+0xPB@users.noreply.github.com>
2024-10-16 14:25:14 +0200

    Update client.c

--- src/client.c
+++ src/client.c
@@ -2,37 +2,36 @@
 
 #define BUFFER_SIZE 1024
 
-char current_channel[50] = ""; // Variable globale pour stocker le salon actuel
+char current_channel[50] = "";
 
 void clean_input(char *str)
 {
     char *pos;
     if ((pos = strchr(str, '\n')) != NULL || (pos = strchr(str, '\r')) != NULL)
     {
-        *pos = '\0'; // Remplacer le retour à la ligne ou retour chariot par un caractère de fin de chaîne
+        *pos = '\0';
     }
 }
 
 void print_message(const char *message, const char *current_input)
 {
-    printf("\r\033[K");      // Effacer la ligne actuelle
-    printf("%s\n", message); // Afficher le message reçu du serveur
+    printf("\r\033[K");      
+    printf("%s\n", message);
 
     if (strlen(current_input) > 0)
     {
-        printf("> %s", current_input); // Réafficher l'entrée utilisateur sauvegardée
+        printf("> %s", current_input);
     }
     else
     {
-        printf("> "); // Réafficher le prompt si aucune entrée utilisateur
+        printf("> ");
     }
 
-    fflush(stdout); // S'assurer que le tampon est vidé
+    fflush(stdout);
 }
 
 void receive_file_from_server(int client_socket, const char *filename)
 {
-    // Réception de la taille du fichier
     char buffer[BUFFER_SIZE];
     int bytes_received = recv(client_socket, buffer, sizeof(buffer) - 1, 0);
     if (bytes_received <= 0)
@@ -41,20 +40,14 @@ void receive_file_from_server(int client_socket, const char *filename)
         return;
     }
     buffer[bytes_received] = '\0';
-    long file_size = atol(buffer); // Convertir la taille en nombre
-
-    // Envoyer la confirmation pour démarrer le transfert
+    long file_size = atol(buffer);
     send(client_socket, "OK", 2, 0);
-
-    // Ouvrir le fichier pour l'écriture
     FILE *file = fopen(filename, "wb");
     if (file == NULL)
     {
         perror("Erreur lors de la création du fichier");
         return;
     }
-
-    // Recevoir les bits du fichier un par un
     char byte;
     long received_bytes = 0;
     while (received_bytes < file_size)
@@ -65,8 +58,6 @@ void receive_file_from_server(int client_socket, const char *filename)
             perror("Erreur lors de la réception du fichier");
             break;
         }
-
-        // Écrire le byte dans le fichier
         fwrite(&byte, 1, 1, file);
         received_bytes++;
     }
@@ -91,22 +82,14 @@ void send_file_to_server(int client_socket, const char *filename)
         perror("Erreur lors de l'ouverture du fichier");
         return;
     }
-
-    // Obtenir la taille du fichier
     fseek(file, 0L, SEEK_END);
     long file_size = ftell(file);
     rewind(file);
-
-    // Envoyer la taille du fichier au serveur
     char size_str[20];
-    snprintf(size_str, sizeof(size_str), "%ld", file_size); // écrire chaîne de caractère dans un buffer pour éviter débordement de mémoire
+    snprintf(size_str, sizeof(size_str), "%ld", file_size);
     send(client_socket, size_str, strlen(size_str), 0);
-
-    // Attendre la confirmation du serveur pour commencer le transfert
     char buffer[BUFFER_SIZE];
     recv(client_socket, buffer, sizeof(buffer), 0);
-
-    // Envoyer le fichier bit par bit
     char byte;
     while (fread(&byte, 1, 1, file) == 1)
     {
@@ -128,7 +111,7 @@ void handle_receive(int client_fd, char *current_input)
     if (bytes_received > 0)
     {
         buffer[bytes_received] = '\0';
-        if (strcmp(buffer, current_input) != 0) // Ne pas réafficher l'entrée utilisateur
+        if (strcmp(buffer, current_input) != 0)
         {
             print_message(buffer, current_input);
         }
@@ -152,7 +135,6 @@ void handle_send(int client_fd, char *current_input)
     fgets(buffer, BUFFER_SIZE, stdin);
     clean_input(buffer);
 
-    // Envoi du message
     if (send(client_fd, buffer, strlen(buffer), 0) < 0)
     {
         perror("Erreur lors de l'envoi du message");
@@ -185,11 +167,7 @@ void handle_send(int client_fd, char *current_input)
         printf("\nSe déconnecter du serveur.\t\t\t\t\tUsage : disconnect\n");
         printf("\n");
     }
-
-    // Sauvegarde de l'entrée utilisateur
     strncpy(current_input, buffer, sizeof(current_input) - 1);
-
-    // Effacer l'entrée utilisateur après l'envoi
     memset(current_input, 0, sizeof(current_input));
 }
 
@@ -198,9 +176,9 @@ int main()
     int client_fd;
     struct sockaddr_in server_addr;
     char username[50], password[50];
-    char current_input[BUFFER_SIZE] = ""; // Pour sauvegarder l'entrée utilisateur
+    char current_input[BUFFER_SIZE] = "";
 
-    client_fd = socket(AF_INET, SOCK_STREAM, 0); // sock_stream - TCP
+    client_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (client_fd < 0)
     {
         perror("socket() failed");
@@ -217,57 +195,40 @@ int main()
         close(client_fd);
         exit(EXIT_FAILURE);
     }
-
-    // Authentification
     printf("Login: ");
     fgets(username, sizeof(username), stdin);
-
-    // Dans client.c, après avoir reçu le mot de passe
     printf("Password: ");
     fgets(password, sizeof(password), stdin);
-
-    // Ensuite, lors de l'envoi
     char auth_info[100];
     snprintf(auth_info, sizeof(auth_info), "%s %s", username, password);
-
-    // S'assurer d'envoyer uniquement la longueur correcte
     send(client_fd, auth_info, strlen(auth_info), 0);
-
-    // Attendre la réponse d'authentification
     char auth_response[BUFFER_SIZE];
     int bytes_received = recv(client_fd, auth_response, sizeof(auth_response) - 1, 0);
     auth_response[bytes_received] = '\0';
-    printf("%s\n", auth_response); // Afficher le message d'authentification
-
-    // Utiliser `poll` pour gérer à la fois les entrées utilisateur et les messages du serveur
+    printf("%s\n", auth_response);
     struct pollfd fds[2];
-    fds[0].fd = STDIN_FILENO; // Entrée utilisateur (stdin)
-    fds[0].events = POLLIN;   // Surveiller les entrées disponibles
-    fds[1].fd = client_fd;    // Socket client
-    fds[1].events = POLLIN;   // Surveiller les messages du serveur
+    fds[0].fd = STDIN_FILENO;
+    fds[0].events = POLLIN;
+    fds[1].fd = client_fd;
+    fds[1].events = POLLIN;
 
     while (1)
     {
-        int poll_count = poll(fds, 2, -1); // Attendre indéfiniment (-1) un événement sur stdin ou le socket client (2)
-        if (poll_count < 0)                // valeur négative, erreur
+        int poll_count = poll(fds, 2, -1);
+        if (poll_count < 0)
         {
             perror("poll() failed");
             break;
         }
-
-        // Vérifier si l'utilisateur a entré une commande
-        if (fds[0].revents & POLLIN) // après l'appel à poll pour indiquer ce qu'il s'est réellement passé
+        if (fds[0].revents & POLLIN)
         {
-            handle_send(client_fd, current_input); // Gérer l'entrée utilisateur et envoyer le message
+            handle_send(client_fd, current_input);
         }
-
-        // Vérifier si des données sont reçues du serveur
         if (fds[1].revents & POLLIN)
         {
-            handle_receive(client_fd, current_input); // Gérer la réception des messages du serveur
+            handle_receive(client_fd, current_input);
         }
     }
-
     close(client_fd);
     return 0;
 }
